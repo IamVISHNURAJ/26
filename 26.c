dc7704d35a5a2a7cb13374cdd9d1e7d5dac1b5eb
IamVISHNURAJ <50854010+IamVISHNURAJ@users.noreply.github.com>
2019-05-23 20:18:24 +0530

    Update 26.c

--- 26.c
+++ 26.c
@@ -1,23 +1,27 @@
-#include <stdio.h>
-int main()
+#include<stdio.h>
+int main( void)
 {
-int i,j,a[20],n,temp;
+int n,a[100],i,k,t;
 scanf("%d",&n);
 for(i=0;i<n;i++)
+{
 scanf("%d",&a[i]);
+}
 for(i=0;i<n;i++)
 {
-for(j=i+1;j<n;j++)
-{
-if(a[i]>a[j])
-{
-temp=a[i];
-a[i]=a[j];
- a[j]=temp;
-}
-}
-}
-for(i=0;i<n;i++)  
-printf("%d ",a[i]);
-return 0;  
-}
+    for(k=i+1;k<n;k++)
+    {
+        if(a[i]>a[k])
+        {
+            t=a[i];
+            a[i]=a[k];
+            a[k]=t;
+        }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+    }
